feba02694c273a40189f95101df4d5f14edee176
Wahyu Ramadan <wramadan1203@gmail.com>
2023-01-12 21:10:08 +0700

    fix sendWrapped

--- src/service.js
+++ src/service.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const httpStatus = require('http-status');
 const dbConfig = require('./configs/db');
 const { errorConverter, errorHandler } = require('./middlewares/errorHandler');
 const routeV1 = require('./routes/index');
+const ApiError = require('./helpers/ApiError');
 
 const app = express();
 
@@ -16,12 +18,24 @@ app.options('*', cors());
 
 app.use('/v1', routeV1);
 
+app.response.sendWrapped = function (message, data, statusCode = httpStatus.OK) {
+  return this.status(statusCode).send({
+    status: statusCode,
+    message,
+    data,
+  });
+};
+
 dbConfig.getConnection((error) => {
   if (error) {
     console.error(error);
   } else {
     console.log('MySQL Database in Connected!');
   }
+});
+
+app.use((req, res, next) => {
+  next(new ApiError('Not found', httpStatus.NOT_FOUND));
 });
 
 app.use(errorConverter);
