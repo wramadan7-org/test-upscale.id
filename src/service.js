e7fd76d85b2657a61fab22df25916a36419d35c1
Wahyu Ramadan <wramadan1203@gmail.com>
2023-01-12 17:24:00 +0700

    connect to database

--- src/service.js
+++ src/service.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dbConfig = require('./configs/db');
 const routeV1 = require('./routes/index');
 
 const app = express();
@@ -13,6 +14,14 @@ app.use(cors());
 app.options('*', cors());
 
 app.use('/v1', routeV1);
+
+dbConfig.connect((error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('MySQL Database in Connected!');
+  }
+});
 
 app.listen(NODE_PORT, () => {
   console.log(`App listen on port ${NODE_PORT}`);
