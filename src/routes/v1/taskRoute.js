b61a9d876e973ff807dc3679dfcf6dd9925a4287
Wahyu Ramadan <wramadan1203@gmail.com>
2023-01-13 10:06:09 +0700

    patch route

--- src/routes/v1/taskRoute.js
+++ src/routes/v1/taskRoute.js
@@ -6,5 +6,7 @@ const router = Router();
 router.post('/', taskController.taskCreateController);
 router.get('/', taskController.taskAllController);
 router.get('/:id', taskController.taskGetByIdController);
+router.patch('/:id', taskController.taskUpdateByIdController);
+router.delete('/:id', taskController.taskDeleteByIdController);
 
 module.exports = router;
